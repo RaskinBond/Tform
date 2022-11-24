8e2569d99cb00899ff805b2d5b1e4db770dd931e
RaskinBond <115020067+RaskinBond@users.noreply.github.com>
2022-11-24 23:13:31 +0530

    Update inputs.tf

--- singapore/inputs.tf
+++ singapore/inputs.tf
@@ -17,14 +17,14 @@ variable "subnets_tags" {
 }
 
 
-//variable "availability_zones" {
-//    type        = list(string)
-//    default     = [ "ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1a", "ap-southeast-1b" ]
-//}
+# variable "availability_zones" {
+#     type        = list(string)
+#     default     = [ "ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1a", "ap-southeast-1b" ]
+# }
 
 
-//variable "subnets_cidr" {
-//    type        = list(string)
-//    default     = [ "10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24" ]
-//}
+# variable "subnets_cidr" {
+#     type        = list(string)
+#     default     = [ "10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24" ]
+# }
 
